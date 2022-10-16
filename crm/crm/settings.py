f1e14a57be39dec1bb1481b67b9111656fbe82ab
Kévin Dérécusson <derec84@hotmail.fr>
2022-10-16 11:40:10 +0200

    Use PostgreSQL database

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "crm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crmEpicEventsDb",
+        "USER": "postgres",
+        "PASSWORD": "changemepassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
