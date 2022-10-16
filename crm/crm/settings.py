663baafbfe7c6f2679871e44ba827f56718b7eae
Kévin Dérécusson <derec84@hotmail.fr>
2022-10-16 10:44:47 +0200

    Fix bad settings
    Apps wasn't correctly installed and urls.py files didn't exist in apps
    Add AUTH_USER_MODEL

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crm.apps.AccountsConfig",
-    "crm.apps.BusinessConfig",
+    "accounts.apps.AccountsConfig",
+    "business.apps.BusinessConfig",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
