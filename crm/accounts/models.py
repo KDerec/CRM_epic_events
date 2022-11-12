2c2de44ebc0c731a83525b3f1f2663561c6eabac
KDerecLaptop <derec8495@gmail.com>
2022-11-12 23:56:22 +0100

    Automatic staff for new user

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=True,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
