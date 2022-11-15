82056ca91dbae954935c1025918201dae1d8f25e
KDerecLaptop <derec8495@gmail.com>
2022-11-15 15:39:48 +0100

    Add group field for user creation and display list

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 from accounts.models import User
 from business.admin import crm_admin_site
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "groupes",
+        "is_staff",
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "groups"),
+            },
+        ),
+    )
+
+    def groupes(self, obj):
+        try:
+            return obj.groups.get()
+        except:
+            return "N/A"
+
 
 crm_admin_site.register(User, UserAdmin)
 admin.site.register(User, UserAdmin)
