e91a0730ee9d517b288f2633493df5b6983bea59
Kévin Dérécusson <derec84@hotmail.fr>
2022-11-17 17:39:56 +0100

    Avoid sales user to choice sales contact of client

--- crm/tests/test_business.py
+++ crm/tests/test_business.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from tests.test_settings import TestData
 
 
@@ -37,6 +38,36 @@ class ClientTestCase(TestData):
             f"/business/client/{self.client_two_sales_user_two.client_id}/change/"
         )
         self.assertEqual(response.context["title"], "Affichage de client")
+
+    def test_manager_can_choice_sales_contact_of_client(self):
+        self.client.force_login(self.manager_user)
+        response = self.client.get("/business/client/add/")
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(soup.find("select", id="id_sales_contact"))
+        response = self.client.get(
+            f"/business/client/{self.client_one_sales_user.client_id}/change/"
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(soup.find("select", id="id_sales_contact"))
+
+    def test_sales_cant_choice_sales_contact_of_client(self):
+        self.client.force_login(self.sales_user)
+        response = self.client.get("/business/client/add/")
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(
+            soup.find("div", class_="form-row field-sales_contact").find(
+                "div", class_="readonly"
+            )
+        )
+        response = self.client.get(
+            f"/business/client/{self.client_one_sales_user.client_id}/change/"
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(
+            soup.find("div", class_="form-row field-sales_contact").find(
+                "div", class_="readonly"
+            )
+        )
 
 
 class EventTestCase(TestData):
