cd3f1e40dc2a9f4052541ed38826a1ee7a92ff18
Kévin Dérécusson <derec84@hotmail.fr>
2022-11-17 09:56:05 +0100

    Change has_change_permission and test it

--- crm/business/admin.py
+++ crm/business/admin.py
@@ -14,23 +14,37 @@ class ClientAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created", "date_updated")
 
     def has_change_permission(self, request, obj=None):
-        try:
-            if request.user.groupsfilter(name="Manager").exists():
-                return True
-            if obj.sales_contact == request.user:
-                return True
-            else:
-                return False
-        except AttributeError:
-            pass
-
-        opts = self.opts
-        codename = get_permission_codename("change", opts)
-        return request.user.has_perm("%s.%s" % (opts.app_label, codename))
+        return can_change_object(self, request, obj)
 
 
 def has_superuser_permission(request):
     return request.user.is_active and request.user.is_superuser
+
+
+def can_change_object(self, request, obj):
+    try:
+        if request.user.groups.filter(name="Manager").exists():
+            return True
+    except AttributeError:
+        pass
+    try:
+        if obj.sales_contact == request.user:
+            return True
+        else:
+            return False
+    except AttributeError:
+        pass
+    try:
+        if obj.support_contact == request.user:
+            return True
+        else:
+            return False
+    except AttributeError:
+        pass
+
+    opts = self.opts
+    codename = get_permission_codename("change", opts)
+    return request.user.has_perm("%s.%s" % (opts.app_label, codename))
 
 
 crm_admin_site = CrmAdminSite(name="crm_admin_site")
--- crm/tests/test_business.py
+++ crm/tests/test_business.py
@@ -1,3 +1,29 @@
 from tests.test_settings import TestData
 from business.models import Client, Contract, Event
 from accounts.models import User
+
+
+class ClientTestCase(TestData):
+    def test_manager_can_change_all_client(self):
+        self.client.force_login(self.manager_user)
+        response = self.client.get("/business/client/3/change/")
+        self.assertEqual(response.context["title"], "Modification de client")
+        response = self.client.get("/business/client/4/change/")
+        self.assertEqual(response.context["title"], "Modification de client")
+
+    def test_support_can_view_client(self):
+        self.client.force_login(self.support_user)
+        response = self.client.get("/business/client/3/change/")
+        self.assertEqual(response.context["title"], "Affichage de client")
+        response = self.client.get("/business/client/4/change/")
+        self.assertEqual(response.context["title"], "Affichage de client")
+
+    def test_sales_can_change_is_assigned_client(self):
+        self.client.force_login(self.sales_user)
+        response = self.client.get("/business/client/3/change/")
+        self.assertEqual(response.context["title"], "Modification de client")
+
+    def test_sales_cant_change_not_assigned_client(self):
+        self.client.force_login(self.sales_user)
+        response = self.client.get("/business/client/4/change/")
+        self.assertEqual(response.context["title"], "Affichage de client")
