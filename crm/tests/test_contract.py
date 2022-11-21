dfa8c06e5c48652326acb00a18b47ed99eef8dd5
Kévin Dérécusson <derec84@hotmail.fr>
2022-11-22 00:52:01 +0100

    Reorganize tests files for site

--- crm/tests/test_contract.py
+++ crm/tests/test_contract.py
@@ -4,9 +4,11 @@ from tests.test_settings import TestData
 from business.models import Contract
 
 
-class ContractTestCase(TestData):
-    def test_manager_can_change_all_contract(self):
+class ContractManagerSiteTestCase(TestData):
+    def setUp(self):
         self.client.force_login(self.manager_user)
+
+    def test_can_change_all_contract(self):
         response = self.client.get(
             f"/business/contract/{self.contract_client_one.contract_id}/change/"
         )
@@ -16,33 +18,7 @@ class ContractTestCase(TestData):
         )
         self.assertEqual(response.context["title"], "Modification de contract")
 
-    def test_support_can_view_contract(self):
-        self.client.force_login(self.support_user)
-        response = self.client.get(
-            f"/business/contract/{self.contract_client_one.contract_id}/change/"
-        )
-        self.assertEqual(response.context["title"], "Affichage de contract")
-        response = self.client.get(
-            f"/business/contract/{self.contract_client_two.contract_id}/change/"
-        )
-        self.assertEqual(response.context["title"], "Affichage de contract")
-
-    def test_sales_can_change_is_assigned_contract(self):
-        self.client.force_login(self.sales_user)
-        response = self.client.get(
-            f"/business/contract/{self.contract_client_one.contract_id}/change/"
-        )
-        self.assertEqual(response.context["title"], "Modification de contract")
-
-    def test_sales_cant_change_not_assigned_contract(self):
-        self.client.force_login(self.sales_user)
-        response = self.client.get(
-            f"/business/contract/{self.contract_client_two.contract_id}/change/"
-        )
-        self.assertEqual(response.context["title"], "Affichage de contract")
-
-    def test_manager_can_choice_sales_contact_of_contract(self):
-        self.client.force_login(self.manager_user)
+    def test_can_choice_sales_contact_of_contract(self):
         response = self.client.get("/business/contract/add/")
         soup = BeautifulSoup(response.content, "html.parser")
         self.assertIsNotNone(soup.find("select", id="id_sales_contact"))
@@ -52,38 +28,7 @@ class ContractTestCase(TestData):
         soup = BeautifulSoup(response.content, "html.parser")
         self.assertIsNotNone(soup.find("select", id="id_sales_contact"))
 
-    def test_sales_cant_choice_sales_contact_of_contract(self):
-        self.client.force_login(self.sales_user)
-        response = self.client.get("/business/contract/add/")
-        soup = BeautifulSoup(response.content, "html.parser")
-        self.assertIsNotNone(
-            soup.find("div", class_="form-row field-sales_contact").find(
-                "div", class_="readonly"
-            )
-        )
-        response = self.client.get(
-            f"/business/contract/{self.contract_client_one.contract_id}/change/"
-        )
-        soup = BeautifulSoup(response.content, "html.parser")
-        self.assertIsNotNone(
-            soup.find("div", class_="form-row field-sales_contact").find(
-                "div", class_="readonly"
-            )
-        )
-
-    def test_sales_cant_choice_not_assigned_client_of_contract(self):
-        self.client.force_login(self.sales_user)
-        response = self.client.get("/business/contract/add/")
-        soup = BeautifulSoup(response.content, "html.parser")
-        client_selection = soup.find("select", id="id_client").find_all("option")
-        client_selection_txt = []
-        for client in client_selection:
-            client_selection_txt.append(client.text)
-
-        self.assertNotIn(self.client_two_sales_user_two.__str__(), client_selection_txt)
-
-    def test_manager_can_choice_all_client_of_contract(self):
-        self.client.force_login(self.manager_user)
+    def test_can_choice_all_client_of_contract(self):
         response = self.client.get("/business/contract/add/")
         soup = BeautifulSoup(response.content, "html.parser")
         client_selection = soup.find("select", id="id_client").find_all("option")
@@ -94,8 +39,7 @@ class ContractTestCase(TestData):
         self.assertIn(self.client_one_sales_user.__str__(), client_selection_txt)
         self.assertIn(self.client_two_sales_user_two.__str__(), client_selection_txt)
 
-    def test_manager_can_choice_all_event_of_contract(self):
-        self.client.force_login(self.manager_user)
+    def test_can_choice_all_event_of_contract(self):
         response = self.client.get("/business/contract/add/")
         soup = BeautifulSoup(response.content, "html.parser")
         event_selection = soup.find("select", id="id_event").find_all("option")
@@ -106,8 +50,52 @@ class ContractTestCase(TestData):
         self.assertIn(self.event_one.__str__(), event_selection_txt)
         self.assertIn(self.event_two.__str__(), event_selection_txt)
 
-    def test_sales_cant_choice_event_of_not_assigned_client_of_contract(self):
+
+class ContractSalesSiteTestCase(TestData):
+    def setUp(self):
         self.client.force_login(self.sales_user)
+
+    def test_can_change_his_assigned_contract(self):
+        response = self.client.get(
+            f"/business/contract/{self.contract_client_one.contract_id}/change/"
+        )
+        self.assertEqual(response.context["title"], "Modification de contract")
+
+    def test_cant_change_not_assigned_contract(self):
+        response = self.client.get(
+            f"/business/contract/{self.contract_client_two.contract_id}/change/"
+        )
+        self.assertEqual(response.context["title"], "Affichage de contract")
+
+    def test_cant_choice_sales_contact_of_contract(self):
+        response = self.client.get("/business/contract/add/")
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(
+            soup.find("div", class_="form-row field-sales_contact").find(
+                "div", class_="readonly"
+            )
+        )
+        response = self.client.get(
+            f"/business/contract/{self.contract_client_one.contract_id}/change/"
+        )
+        soup = BeautifulSoup(response.content, "html.parser")
+        self.assertIsNotNone(
+            soup.find("div", class_="form-row field-sales_contact").find(
+                "div", class_="readonly"
+            )
+        )
+
+    def test_cant_choice_not_assigned_client_for_contract(self):
+        response = self.client.get("/business/contract/add/")
+        soup = BeautifulSoup(response.content, "html.parser")
+        client_selection = soup.find("select", id="id_client").find_all("option")
+        client_selection_txt = []
+        for client in client_selection:
+            client_selection_txt.append(client.text)
+
+        self.assertNotIn(self.client_two_sales_user_two.__str__(), client_selection_txt)
+
+    def test_cant_choice_event_of_not_assigned_client_for_contract(self):
         response = self.client.get("/business/contract/add/")
         soup = BeautifulSoup(response.content, "html.parser")
         event_selection = soup.find("select", id="id_event").find_all("option")
@@ -118,7 +106,7 @@ class ContractTestCase(TestData):
         self.assertIn(self.event_one.__str__(), event_selection_txt)
         self.assertNotIn(self.event_two.__str__(), event_selection_txt)
 
-    def test_create_contract_with_client_and_event_of_different_client(self):
+    def test_cant_create_contract_with_client_and_event_of_another_client(self):
         with self.assertRaises(ValidationError):
             Contract(
                 status=False,
@@ -128,3 +116,18 @@ class ContractTestCase(TestData):
                 sales_contact=self.sales_user,
                 event=self.event_two,
             ).full_clean()
+
+
+class ContractSupportSiteTestCase(TestData):
+    def setUp(self):
+        self.client.force_login(self.support_user)
+
+    def test_can_view_contract(self):
+        response = self.client.get(
+            f"/business/contract/{self.contract_client_one.contract_id}/change/"
+        )
+        self.assertEqual(response.context["title"], "Affichage de contract")
+        response = self.client.get(
+            f"/business/contract/{self.contract_client_two.contract_id}/change/"
+        )
+        self.assertEqual(response.context["title"], "Affichage de contract")
