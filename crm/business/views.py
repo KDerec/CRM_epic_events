a32bcc7d8ec3ce3e4b5a00205a73dd809299adc7
Kévin Dérécusson <derec84@hotmail.fr>
2022-11-25 01:56:06 +0100

    Simplify code with dict and function

--- crm/business/views.py
+++ crm/business/views.py
@@ -45,31 +45,33 @@ class ClientViewSet(viewsets.ModelViewSet):
             return super().get_serializer_class()
 
     def create(self, request, *args, **kwargs):
+        data_dict = {}
         try:
-            sales_contact = request.data["sales_contact"]
+            data_dict["sales_contact"] = request.data["sales_contact"]
         except MultiValueDictKeyError:
-            sales_contact = "is_empty"
+            data_dict["sales_contact"] = "is_empty"
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer, sales_contact)
+        self.perform_create(serializer, data_dict)
         headers = self.get_success_headers(serializer.data)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
 
-    def perform_create(self, serializer, sales_contact):
-        save_serializer_for_client_object(self, serializer, sales_contact)
+    def perform_create(self, serializer, data_dict):
+        save_serializer_for_client_object(self, serializer, data_dict)
 
     def update(self, request, *args, **kwargs):
+        data_dict = {}
         try:
-            sales_contact = request.data["sales_contact"]
+            data_dict["sales_contact"] = request.data["sales_contact"]
         except MultiValueDictKeyError:
-            sales_contact = "is_empty"
+            data_dict["sales_contact"] = "is_empty"
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer, sales_contact)
+        self.perform_update(serializer, data_dict)
         serializer.save()
 
         if getattr(instance, "_prefetched_objects_cache", None):
@@ -79,8 +81,8 @@ class ClientViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    def perform_update(self, serializer, sales_contact):
-        save_serializer_for_client_object(self, serializer, sales_contact)
+    def perform_update(self, serializer, data_dict):
+        save_serializer_for_client_object(self, serializer, data_dict)
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -115,24 +117,25 @@ class EventViewSet(viewsets.ModelViewSet):
             return super().get_serializer_class()
 
     def create(self, request, *args, **kwargs):
+        data_dict = {}
         try:
-            client = request.data["client"]
+            data_dict["client"] = request.data["client"]
         except MultiValueDictKeyError:
-            client = "is_empty"
+            data_dict["client"] = "is_empty"
         try:
-            support_contact = request.data["support_contact"]
+            data_dict["support_contact"] = request.data["support_contact"]
         except MultiValueDictKeyError:
-            support_contact = "is_empty"
+            data_dict["support_contact"] = "is_empty"
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer, client, support_contact)
+        self.perform_create(serializer, data_dict)
         headers = self.get_success_headers(serializer.data)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
 
-    def perform_create(self, serializer, client, support_contact):
-        save_serializer_for_event_object(self, serializer, client, support_contact)
+    def perform_create(self, serializer, data_dict):
+        save_serializer_for_event_object(self, serializer, data_dict)
 
     def update(self, request, *args, **kwargs):
         if self.request.user.groups.filter(name="Support").exists():
@@ -149,19 +152,20 @@ class EventViewSet(viewsets.ModelViewSet):
             except MultiValueDictKeyError:
                 pass
             return super().update(request, *args, **kwargs)
+        data_dict = {}
         try:
-            client = request.data["client"]
+            data_dict["client"] = request.data["client"]
         except MultiValueDictKeyError:
-            client = "is_empty"
+            data_dict["client"] = "is_empty"
         try:
-            support_contact = request.data["support_contact"]
+            data_dict["support_contact"] = request.data["support_contact"]
         except MultiValueDictKeyError:
-            support_contact = "is_empty"
+            data_dict["support_contact"] = "is_empty"
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer, client, support_contact)
+        self.perform_update(serializer, data_dict)
 
         if getattr(instance, "_prefetched_objects_cache", None):
             # If 'prefetch_related' has been applied to a queryset, we need to
@@ -170,11 +174,11 @@ class EventViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    def perform_update(self, serializer, client=None, support_contact=None):
+    def perform_update(self, serializer, data_dict=None):
         if self.request.user.groups.filter(name="Support").exists():
             return super().perform_update(serializer)
         else:
-            save_serializer_for_event_object(self, serializer, client, support_contact)
+            save_serializer_for_event_object(self, serializer, data_dict)
 
 
 class ContractViewSet(viewsets.ModelViewSet):
@@ -204,49 +208,49 @@ class ContractViewSet(viewsets.ModelViewSet):
             return super().get_serializer_class()
 
     def create(self, request, *args, **kwargs):
+        data_dict = {}
         try:
-            sales_contact = request.data["sales_contact"]
+            data_dict["sales_contact"] = request.data["sales_contact"]
         except MultiValueDictKeyError:
-            sales_contact = "is_empty"
+            data_dict["sales_contact"] = "is_empty"
         try:
-            client = request.data["client"]
+            data_dict["client"] = request.data["client"]
         except MultiValueDictKeyError:
-            client = "is_empty"
+            data_dict["client"] = "is_empty"
         try:
-            event = request.data["event"]
+            data_dict["event"] = request.data["event"]
         except MultiValueDictKeyError:
-            event = "is_empty"
+            data_dict["event"] = "is_empty"
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer, sales_contact, client, event)
+        self.perform_create(serializer, data_dict)
         headers = self.get_success_headers(serializer.data)
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
 
-    def perform_create(self, serializer, sales_contact, client, event):
-        save_serializer_for_contract_object(
-            self, serializer, sales_contact, client, event
-        )
+    def perform_create(self, serializer, data_dict):
+        save_serializer_for_contract_object(self, serializer, data_dict)
 
     def update(self, request, *args, **kwargs):
+        data_dict = {}
         try:
-            sales_contact = request.data["sales_contact"]
+            data_dict["sales_contact"] = request.data["sales_contact"]
         except MultiValueDictKeyError:
-            sales_contact = "is_empty"
+            data_dict["sales_contact"] = "is_empty"
         try:
-            client = request.data["client"]
+            data_dict["client"] = request.data["client"]
         except MultiValueDictKeyError:
-            client = "is_empty"
+            data_dict["client"] = "is_empty"
         try:
-            event = request.data["event"]
+            data_dict["event"] = request.data["event"]
         except MultiValueDictKeyError:
-            event = "is_empty"
+            data_dict["event"] = "is_empty"
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer, sales_contact, client, event)
+        self.perform_update(serializer, data_dict)
 
         if getattr(instance, "_prefetched_objects_cache", None):
             # If 'prefetch_related' has been applied to a queryset, we need to
@@ -255,67 +259,68 @@ class ContractViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
-    def perform_update(self, serializer, sales_contact, client, event):
-        save_serializer_for_contract_object(
-            self, serializer, sales_contact, client, event
-        )
+    def perform_update(self, serializer, data_dict):
+        save_serializer_for_contract_object(self, serializer, data_dict)
 
 
-def save_serializer_for_client_object(self, serializer, sales_contact):
+def check_value_isnt_empty(dict):
+    for key, value in dict.items():
+        if value == "is_empty":
+            raise ValidationError(f"Veuillez renseigner un {key}.")
+
+
+def save_serializer_for_client_object(self, serializer, data_dict):
     if self.request.user.groups.filter(name="Sales").exists():
-        if sales_contact != "is_empty":
+        if data_dict["sales_contact"] != "is_empty":
             raise ValidationError(
                 "Vous ne pouvez pas modifier le champ 'sales_contact', veuillez le retirer de votre requête."
             )
         serializer.save(sales_contact=self.request.user)
-
-    elif sales_contact == "is_empty":
-        raise ValidationError("Veuillez renseigner un sales contact.")
-
-    elif User.objects.filter(username=sales_contact).exists():
-        serializer.save(sales_contact=User.objects.get(username=sales_contact))
-
-    elif "http" in sales_contact:
-        sales_contact_id = sales_contact.split("/")[-2]
-        serializer.save(sales_contact=User.objects.get(id=sales_contact_id))
-
     else:
-        raise ValidationError(
-            "Veuillez renseigner un 'username' de 'sales contact' existant."
-        )
+        check_value_isnt_empty(data_dict)
+
+        if User.objects.filter(username=data_dict["sales_contact"]).exists():
+            serializer.save(
+                sales_contact=User.objects.get(username=data_dict["sales_contact"])
+            )
+
+        elif "http" in data_dict["sales_contact"]:
+            sales_contact_id = data_dict["sales_contact"].split("/")[-2]
+            serializer.save(sales_contact=User.objects.get(id=sales_contact_id))
+
+        else:
+            raise ValidationError(
+                "Veuillez renseigner un 'username' de 'sales contact' existant."
+            )
 
 
-def save_serializer_for_event_object(self, serializer, client, support_contact):
-    if client == "is_empty":
-        raise ValidationError("Veuillez renseigner un contract.")
-
-    elif support_contact == "is_empty":
-        raise ValidationError("Veuillez renseigner un support contact.")
-
-    elif (
-        User.objects.filter(username=support_contact).exists()
-        and Client.objects.filter(email=client).exists()
+def save_serializer_for_event_object(self, serializer, data_dict):
+    check_value_isnt_empty(data_dict)
+    if (
+        User.objects.filter(username=data_dict["support_contact"]).exists()
+        and Client.objects.filter(email=data_dict["client"]).exists()
     ):
         if (
             self.request.user.groups.filter(name="Sales").exists()
-            and Client.objects.get(email=client).sales_contact != self.request.user
+            and Client.objects.get(email=data_dict["client"]).sales_contact
+            != self.request.user
         ):
             raise ValidationError("Veuillez choisir un client qui vous appartient.")
         serializer.save(
-            support_contact=User.objects.get(username=support_contact),
-            client=Client.objects.get(email=client),
+            support_contact=User.objects.get(username=data_dict["support_contact"]),
+            client=Client.objects.get(email=data_dict["client"]),
         )
-    elif "http" in support_contact:
-        support_contact_id = support_contact.split("/")[-2]
-        client_id = client.split("/")[-2]
+    elif "http" in data_dict["support_contact"]:
+        support_contact_id = data_dict["support_contact"].split("/")[-2]
+        client_id = data_dict["client"].split("/")[-2]
         serializer.save(
             support_contact=User.objects.get(id=support_contact_id),
             client=Client.objects.get(client_id=client_id),
         )
     else:
         try:
-            Client.objects.get(email=client)
-            User.objects.get(username=support_contact)
+            Client.objects.get(email=data_dict["client"])
+            User.objects.get(username=data_dict["support_contact"])
         except User.DoesNotExist:
             raise ValidationError(
                 "Veuillez renseigner un 'username' de 'support contact' existant."
@@ -326,22 +331,19 @@ def save_serializer_for_event_object(self, serializer, client, support_contact):
             )
 
 
-def save_serializer_for_contract_object(self, serializer, sales_contact, client, event):
+def save_serializer_for_contract_object(self, serializer, data_dict):
 
     if self.request.user.groups.filter(name="Sales").exists():
-        if sales_contact != "is_empty":
+        if data_dict["sales_contact"] != "is_empty":
             raise ValidationError(
                 "Vous ne pouvez pas modifier le champ 'sales_contact', veuillez le retirer de votre requête."
             )
-        elif client == "is_empty":
-            raise ValidationError("Veuillez renseigner un client.")
-
-        elif event == "is_empty":
-            raise ValidationError("Veuillez renseigner un event.")
+        data_dict.pop("sales_contact")
+        check_value_isnt_empty(data_dict)
         try:
-            int(event)
-            client = Client.objects.get(email=client)
-            event = Event.objects.get(event_id=event)
+            int(data_dict["event"])
+            client = Client.objects.get(email=data_dict["client"])
+            event = Event.objects.get(event_id=data_dict["event"])
         except ValueError:
             raise ValueError("Veuillez renseigner un numéro d'id pour event.")
         except Client.DoesNotExist:
@@ -365,51 +367,47 @@ def save_serializer_for_contract_object(self, serializer, sales_contact, client,
                 event=event,
             )
 
-    elif sales_contact == "is_empty":
-        raise ValidationError("Veuillez renseigner un sales contact.")
-
-    elif client == "is_empty":
-        raise ValidationError("Veuillez renseigner un client.")
-
-    elif event == "is_empty":
-        raise ValidationError("Veuillez renseigner un event.")
-
-    elif (
-        User.objects.filter(username=sales_contact).exists()
-        and Client.objects.filter(email=client).exists()
-        and Event.objects.filter(event_id=event)
-    ):
-        serializer.save(
-            sales_contact=User.objects.get(username=sales_contact),
-            client=Client.objects.get(email=client),
-            event=Event.objects.get(event_id=event),
-        )
-
-    elif "http" in sales_contact:
-        sales_contact_id = sales_contact.split("/")[-2]
-        client_id = client.split("/")[-2]
-        event_id = event.split("/")[-2]
-        serializer.save(
-            sales_contact=User.objects.get(id=sales_contact_id),
-            client=Client.objects.get(client_id=client_id),
-            event=Event.objects.get(event_id=event_id),
-        )
-
     else:
-        try:
-            int(event)
-            User.objects.get(username=sales_contact)
-            Client.objects.get(email=client)
-            Event.objects.get(event_id=event)
-        except ValueError:
-            raise ValueError("Veuillez renseigner un numéro d'id pour event.")
-        except User.DoesNotExist:
-            raise ValidationError(
-                "Veuillez renseigner un 'username' de 'sales contact' existant."
+        check_value_isnt_empty(data_dict)
+
+        if (
+            User.objects.filter(username=data_dict["sales_contact"]).exists()
+            and Client.objects.filter(email=data_dict["client"]).exists()
+            and Event.objects.filter(event_id=data_dict["event"])
+        ):
+            serializer.save(
+                sales_contact=User.objects.get(username=data_dict["sales_contact"]),
+                client=Client.objects.get(email=data_dict["client"]),
+                event=Event.objects.get(event_id=data_dict["event"]),
             )
-        except Client.DoesNotExist:
-            raise ValidationError(
-                "Veuillez renseigner un 'email' de 'client' existant."
+
+        elif "http" in data_dict["sales_contact"]:
+            sales_contact_id = data_dict["sales_contact"].split("/")[-2]
+            client_id = data_dict["client"].split("/")[-2]
+            event_id = data_dict["event"].split("/")[-2]
+            serializer.save(
+                sales_contact=User.objects.get(id=sales_contact_id),
+                client=Client.objects.get(client_id=client_id),
+                event=Event.objects.get(event_id=event_id),
             )
-        except Event.DoesNotExist:
-            raise ValidationError("Veuillez renseigner un 'id' de 'event' existant.")
+
+        else:
+            try:
+                int(data_dict["event"])
+                User.objects.get(username=data_dict["sales_contact"])
+                Client.objects.get(email=data_dict["client"])
+                Event.objects.get(event_id=data_dict["event"])
+            except ValueError:
+                raise ValueError("Veuillez renseigner un numéro d'id pour event.")
+            except User.DoesNotExist:
+                raise ValidationError(
+                    "Veuillez renseigner un 'username' de 'sales contact' existant."
+                )
+            except Client.DoesNotExist:
+                raise ValidationError(
+                    "Veuillez renseigner un 'email' de 'client' existant."
+                )
+            except Event.DoesNotExist:
+                raise ValidationError(
+                    "Veuillez renseigner un 'id' de 'event' existant."
+                )
